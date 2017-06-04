ee8abaafd16fd06e19297101fafdd21bfaad55ab
BigJMoney <pipplepopple@gmail.com>
2017-06-04 19:15:06 -0400

    Some tutorial fun

--- commands/command.py
+++ commands/command.py
@@ -31,6 +31,27 @@ class Command(BaseCommand):
     """
     pass
 
+class CmdAbilities(Command):
+     """
+     List abilities
+
+     Usage:
+     abilities
+
+     Displays a list of your current ability values.
+     """
+     key = "abilities"
+     aliases = ["abi"]
+     lock = "cmd:all()"
+     help_category = "General"
+
+     def func(self):
+         "implements the actual functionality"
+
+         str, agi, mag = self.caller.get_abilities()
+         string = "STR: {}, AGI: {}, MAG: {}".format(str, agi, mag)
+         self.caller.msg(string)
+
 # -------------------------------------------------------------
 #
 # The default commands inherit from
