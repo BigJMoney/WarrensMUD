8cbd7bcf5524e7b5f558db042e3095844e5f6ee1
BigJMoney <pipplepopple@gmail.com>
2017-06-05 15:03:51 -0400

    Cleaning up errors that occur when developing on multiple PCs

--- launch wMUD.bat
+++ launch wMUD.bat
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-cmd /c "activate.bat && evennia start -l"
-=======
 cmd /k "..\pyenv\scripts\activate.bat && evennia start -l"
->>>>>>> 3cd75c8... Cleaning up errors that occur when developing on multiple PCs
